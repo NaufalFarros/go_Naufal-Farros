9f6f5e6b39a4c56c522d4d91adef6c35390bd3eb
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2023-02-18 00:20:06 +0700

    Create Luas-Trapesium.go

---
+++ 5_Basic Programming/Praktikum/Luas-Trapesium.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  var alas, tinggi, sisiAtas float64
+
+	fmt.Println("Masukkan alas:")
+	fmt.Scan(&alas)
+
+	fmt.Println("Masukkan tinggi:")
+	fmt.Scan(&tinggi)
+
+	fmt.Println("Masukkan sisiAtas:")
+	fmt.Scan(&sisiAtas)
+
+	var luas float64 = (alas + sisiAtas) * tinggi / 2
+
+	fmt.Println("Luas trapesium adalah:", luas)
+
+}
